5d989b4015a756f245704365694285cc40a0aef2
Tran Van Duy <tranvanduy28032001@gmail.com>
2022-10-05 10:49:06 +0700

    Remove Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Button from '~/components/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button />
+
       </header>
     </div>
   );

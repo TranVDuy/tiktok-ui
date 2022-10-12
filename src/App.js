94cabad5760430df3f0516dee72678c6342a641e
Tran Van Duy <tranvanduy28032001@gmail.com>
2022-10-12 20:21:20 +0700

    Design Layouts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { publicRoute } from '~/routes'
+import { DefaultLayout } from '~/components/Layout'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <Routes>
+        {
+          publicRoute.map((route, index) => {
+            // const Layout = route.layout === null ? Fragment : route.layout
+            let Layout = DefaultLayout
 
-      </header>
-    </div>
+            if(route.layout != null){
+              Layout = route.layout
+            }
+            else{
+              if(route.layout === null)
+                Layout = Fragment
+            }
+
+            const Page = route.component
+            return <Route key={index} path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+            />
+          })}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// layouts
+import { HeaderOnly } from '~/components/Layout'
+// import { DefaultLayout } from '~/components/Layout'
+
+// Pages
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+// public route
+const publicRoute = [
+    { path: '/', component: Home  },
+    { path: '/following', component: Following},
+    { path: '/profile', component: Profile},
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+]
+
+
+export { publicRoute }

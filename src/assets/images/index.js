01a833c17e683d95a129d01ab82f57fef89bea64
Tran Van Duy <tranvanduy28032001@gmail.com>
2022-10-13 08:25:16 +0700

    Create Logo

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    src: '~/assets/images/logo-tiktok.png'
+}
+
+export default images
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,20 @@
 import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
-
+import images from '~/assets/images'
 
 const cx = classNames.bind(styles)
 
 function Header() {
+    console.log(images.src);
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* Logo */}
-                {/* Search */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Logo Tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Search accounts and videos' spellCheck={true} />
+                </div>
             </div>
         </header>
     )

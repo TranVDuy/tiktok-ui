839d46a4ecd06327c8b760d5c025a231ef591558
Tran Van Duy <tranvanduy28032001@gmail.com>
2022-10-13 14:55:45 +0700

    CSS Search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -2,6 +2,10 @@ import styles from './Header.module.scss'
 import classNames from 'classnames/bind';
 import images from '~/assets/images'
 
+// Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 const cx = classNames.bind(styles)
 
 function Header() {
@@ -9,11 +13,24 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+                {/* Logo */}
                 <div className={cx('logo')}>
                     <img src={images.logo} alt="Logo Tiktok" />
                 </div>
+                {/* Search */}
                 <div className={cx('search')}>
                     <input placeholder='Search accounts and videos' spellCheck={true} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                {/* Action */}
+                <div className={cx('action')}>
+
                 </div>
             </div>
         </header>
